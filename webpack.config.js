ad010ba60be5f4d82bca72d7c0dbab903a1cf16f
Pavel <pavelpetrochenko2001@gmail.com>
2021-11-06 05:08:25 +0300

    add template.html

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'deadbit-dev',
+            template: './template.html'
         }),
     ],
     mode: 'production',
