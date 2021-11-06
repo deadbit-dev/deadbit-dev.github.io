10b28c69f7cc4942293a1d75bb5f7b6f554a4309
Pavel <pavelpetrochenko2001@gmail.com>
2021-11-06 04:20:08 +0300

    push build

--- src/components/App.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { Screen } from "./Screen";
-
-export const App = () => ( <Screen /> );
--- src/components/Screen.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Box } from "./Box";
-
-import "./style.css";
-
-export const Screen = () => ( <Box /> );
