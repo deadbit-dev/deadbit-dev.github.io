0527642e9e6bb0d3c7072e4568dc5db2b47e2282
Pavel <pavelpetrochenko2001@gmail.com>
2021-11-14 18:23:50 +0300

    refactoring and added tsparticles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Screen } from "./Screen";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/style.css";
+
 export const App = () => (
     <Screen />
 );
--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,32 +1,11 @@
 import React from 'react';
-//import Container from 'react-bootstrap/Container';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
-import { ParallaxScene, ParallaxLayer } from './ParallaxComponent';
+import { Background } from './Background';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./style.css";
 
 export const Screen = () => {
-    const parallaxConf = {
-        // TODO: parametrs for scene
-        pointerEvents: true
-    };
-
     return (
         <div id="screen">
-                    <ParallaxScene config={parallaxConf}>
-                <ParallaxLayer depth="0.5" />
-                <ParallaxLayer depth="1.0">
-                    <div className="d-flex justify-content-center" style={{"height": "100vh"}}>
-                        <div 
-                            className="align-self-end bg-image" 
-                            style={{"width": "100vw", "height": "50vh"}} 
-                            onClick={()=> alert("click")}
-                        />
-                    </div>
-                </ParallaxLayer>
-            </ParallaxScene>
+            <Background />
         </div>
     );
 };
