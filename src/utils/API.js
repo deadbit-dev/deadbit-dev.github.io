ddfe2e81bf35da3d52752a2ab08d98697c4073a8
Pavel <pavelpetrochenko2001@gmail.com>
2021-11-22 15:21:59 +0300

    refactoring

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,3 +1,5 @@
+import { Vector3 } from "three";
+
 export const API = () => {
     return {
         'cpp': {
@@ -19,31 +21,31 @@ export const API = () => {
 
 export const APIParser = (data, nodes) => {
     let result = new Array();
-    let scaleX = 1;
-    let scaleY = 1;
-    let scaleZ = 1;
-    let posX = 0;
-    let posY = 0;
-    let posZ = 0;
-    const sizeX = Math.abs(nodes.Cube.geometry.boundingBox.min.x - nodes.Cube.geometry.boundingBox.max.x);
-    const sizeY = nodes.Cube.geometry.boundingBox.max.y;
+    let scale = new Vector3(1, 1, 1);
+    let pos = new Vector3(0, 0, 0);
+    const sizeX = Math.abs(nodes.Cube.geometry.boundingBox.min.x - nodes.Cube.geometry.boundingBox.max.x) * scale.x;
+    const sizeY = Math.abs(nodes.Cube.geometry.boundingBox.min.y - nodes.Cube.geometry.boundingBox.max.y) * scale.y;
+    const halfX = nodes.Cube.geometry.boundingBox.max.x * scale.x;
+    const halfY = nodes.Cube.geometry.boundingBox.max.y * scale.y;
+    const startX = nodes.Hex.geometry.boundingBox.min.x;
+    const startY = nodes.Hex.geometry.boundingBox.max.y;
     const distanceX = sizeX / 2;
     const distanceY = 0;
     const dataArray = Object.entries(data);
     dataArray.forEach((lang, langIndex) => {
         const weightsArray = Object.entries(lang[1]);
-        posX = nodes.Hex.geometry.boundingBox.min.x + langIndex * (scaleX * sizeX + distanceX) + distanceX * 2;
-        posY = nodes.Hex.geometry.boundingBox.max.y;
+        const offsetX = langIndex * (sizeX + distanceX);
+        pos.setX(startX + distanceX + offsetX + halfX);
+        pos.setY(startY);
         weightsArray.forEach((repo, repoIndex) => {
             const weight = repo[1];
-            scaleY = weight / 10;
-            posY += scaleY * sizeY + distanceY * repoIndex;
+            scale.setY(weight / 10); // FIXME: weight
+            pos.setY(pos.y + scale.y * sizeY + distanceY * repoIndex);
             result.push({
                 'key': langIndex.toString() + repoIndex.toString(),
-                'position': [posX, posY, posZ],
-                'scale': [scaleX, scaleY, scaleZ]
+                'position': [pos.x, pos.y - scale.y * halfY, pos.z],
+                'scale': [scale.x, scale.y, scale.z]
             });
-            posY += scaleY * sizeY;
         });
     });
     return result;
