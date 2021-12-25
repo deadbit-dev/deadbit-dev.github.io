e1c16180922a75fb39e1802d66fbe6fa7148fcce
Pavel <pavelpetrochenko2001@gmail.com>
2021-12-25 14:37:04 +0300

    hot fix

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -7,32 +7,35 @@ const Scene = lazy(() => import("./Scene"));
 
 export default function Screen() {
     const ref = useRef();
-    const [angle, setAngle] = useState(window.screen.orientation.angle);
-    window.screen.orientation.onchange = (event) => {
-        setAngle(event.currentTarget.angle);
-    };
+    
+    if(window.screen.orientation){
+        const [angle, setAngle] = useState(window.screen.orientation.angle);
+        window.screen.orientation.onchange = (event) => {
+            setAngle(event.currentTarget.angle);
+        };
 
-    useEffect(() => {
-        switch(angle){
-            case 0:
-                ref.current.style.display = "block";
-                document.body.classList.remove("turnDeviceNotification");
-                break;
-            case 180:
-                ref.current.style.display = "block";
-                document.body.classList.remove("turnDeviceNotification");
-                break;
-            case 90:
-                ref.current.style.display = "none";
-                document.body.classList.add("turnDeviceNotification");
-                break;
-            case -90:
-                ref.current.style.display = "none";
-                document.body.classList.add("turnDeviceNotification");
-                break;
-        }
-    });
-
+        useEffect(() => {
+            switch(angle){
+                case 0:
+                    ref.current.style.display = "block";
+                    document.body.classList.remove("turnDeviceNotification");
+                    break;
+                case 180:
+                    ref.current.style.display = "block";
+                    document.body.classList.remove("turnDeviceNotification");
+                    break;
+                case 90:
+                    ref.current.style.display = "none";
+                    document.body.classList.add("turnDeviceNotification");
+                    break;
+                case -90:
+                    ref.current.style.display = "none";
+                    document.body.classList.add("turnDeviceNotification");
+                    break;
+            }
+        });
+    }
+    
     return (
         <div 
             ref={ref}
